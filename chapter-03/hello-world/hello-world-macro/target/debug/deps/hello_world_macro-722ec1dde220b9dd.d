8954fd3020ef1018f6bd431aac36f40e1f63e4ce
azalhu-x <159701700+azalhu-x@users.noreply.github.com>
2025-02-04 21:12:27 +0100

    Delete chapter-03/hello-world/hello-world-macro/target directory

--- chapter-03/hello-world/hello-world-macro/target/debug/deps/hello_world_macro-722ec1dde220b9dd.d
+++
@@ -1,5 +0,0 @@
-/Users/azalhu/dev/rust/write-powerful-rust-macros/chapter-03/hello-world/hello-world-macro/target/debug/deps/libhello_world_macro-722ec1dde220b9dd.rmeta: src/lib.rs
-
-/Users/azalhu/dev/rust/write-powerful-rust-macros/chapter-03/hello-world/hello-world-macro/target/debug/deps/hello_world_macro-722ec1dde220b9dd.d: src/lib.rs
-
-src/lib.rs:
--- chapter-03/hello-world/hello-world-macro/target/debug/deps/hello_world_macro-b417d7289cd67d54.d
+++
@@ -1,5 +0,0 @@
-/Users/azalhu/dev/rust/write-powerful-rust-macros/chapter-03/hello-world/hello-world-macro/target/debug/deps/libhello_world_macro-b417d7289cd67d54.rmeta: src/lib.rs
-
-/Users/azalhu/dev/rust/write-powerful-rust-macros/chapter-03/hello-world/hello-world-macro/target/debug/deps/hello_world_macro-b417d7289cd67d54.d: src/lib.rs
-
-src/lib.rs:
